f687afa72d13f8a25086252551978ff6da5a0c80
hc2124 <hc2124@jagmail.southalabama.edu>
2025-10-23 13:19:41 -0500

    minor changes
    nothing crazy changed, removed some unnecessary files

--- TriviaAuthApi/Data/AppDb.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-public class AppDb : IdentityDbContext
-{
-    public AppDb(DbContextOptions<AppDb> options) : base(options) { }
-}
